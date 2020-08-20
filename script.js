26b6750d972b987efb54e9525d7e20b40621c3b2
Rob Kellen <rob.h.kellen@gmail.com>
2020-08-20 14:18:10 -0700

    functions working to display comic cover based on user character search

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function myFunction() {
   }
 }
 
-var charID;
-var searchName = $("#searchEntry").val().trim();
+//declare name of character searched from HTML input
+const searchName = $("#searchEntry").val().trim();
 
 function characterSearch (){
   const hash = "f3ca3e169c9bd8ca02d93185989ba9cc";
@@ -22,29 +22,29 @@ function characterSearch (){
     method: "GET",
   }).then(function (response) {
     console.log(response);
-    const charID = response.data.results[0].id  
-    console.log(charID);
+    comicSearch(response.data.results[0].id) 
   });  
-  comicSearch();
-  function comicSearch(){
-    const hash = "f3ca3e169c9bd8ca02d93185989ba9cc";
-    const apiKey = "6ac5ff42f16a138a51581025d4b3838f";
-    const charID = characterSearch(charID);
-    const queryUrl2 = "http://gateway.marvel.com/v1/public/characters/" + charID + "/comics?&ts=1&apikey=6ac5ff42f16a138a51581025d4b3838f&hash=f3ca3e169c9bd8ca02d93185989ba9cc";
   
-    $.ajax({
-      url: queryUrl2,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response);
-      const coverThumbUrl = response.data.results[0].thumbnail.path;
-      console.log(coverThumbUrl);
-      const coverImage = coverThumbUrl + ".jpg";
-      $("#test").append($("<img>").attr("src", coverImage));
-    });   
-  }
 }
-// characterSearch();
+
+
+function comicSearch(charID){
+  const hash = "f3ca3e169c9bd8ca02d93185989ba9cc";
+  const apiKey = "6ac5ff42f16a138a51581025d4b3838f";
+  // const charID = "";
+  const queryUrl2 = "http://gateway.marvel.com/v1/public/characters/" + charID + "/comics?&ts=1&apikey=6ac5ff42f16a138a51581025d4b3838f&hash=f3ca3e169c9bd8ca02d93185989ba9cc";
+
+  $.ajax({
+    url: queryUrl2,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    const coverThumbUrl = response.data.results[0].thumbnail.path;
+    console.log(coverThumbUrl);
+    const coverImage = coverThumbUrl + ".jpg";
+    $("#test").append($("<img>").attr("src", coverImage));
+  });   
+}
 
 
 
