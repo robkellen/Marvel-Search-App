3c81a9f350d4cce653e7c85353ffb58f1f4f1db1
Rob Kellen <rob.h.kellen@gmail.com>
2020-08-19 18:23:29 -0700

    working function to create character ID based off of character name search.  Function created to obtain comics for each character and thumbnails for the comic covers

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -8,28 +7,44 @@ function myFunction() {
   }
 }
 
-=======
+const charID = "";
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
->>>>>>> 1c67971d9e2d9a20262e182831dba28ed5e5878b
 
-const hash = "f3ca3e169c9bd8ca02d93185989ba9cc"
+function characterSearch (){
+  const hash = "f3ca3e169c9bd8ca02d93185989ba9cc";
+  const apiKey = "6ac5ff42f16a138a51581025d4b3838f";
+  const searchName = "hulk"
+  //queryUrl built with md5
+  const queryUrl = "http://gateway.marvel.com/v1/public/characters?name=" + searchName + "&ts=1&apikey=6ac5ff42f16a138a51581025d4b3838f&hash=f3ca3e169c9bd8ca02d93185989ba9cc";  
+  
+  $.ajax({
+    url: queryUrl,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    const charID = response.data.results[0].id  
+    console.log(charID);
+    return charID;
+  });  
+}
+//characterSearch();
 
-const apiKey = "6ac5ff42f16a138a51581025d4b3838f"
-//queryUrl built with md5 
-const queryUrl = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=6ac5ff42f16a138a51581025d4b3838f&hash=f3ca3e169c9bd8ca02d93185989ba9cc";
 
-$.ajax({
-  url: queryUrl,
-  method: "GET"
-}).then(function(response){
-  console.log(response);
-});
 
+function comicSearch(){
+  const hash = "f3ca3e169c9bd8ca02d93185989ba9cc";
+  const apiKey = "6ac5ff42f16a138a51581025d4b3838f";
+  const charID = "1009351"
+  const queryUrl2 = "http://gateway.marvel.com/v1/public/characters/" + charID + "/comics?&ts=1&apikey=6ac5ff42f16a138a51581025d4b3838f&hash=f3ca3e169c9bd8ca02d93185989ba9cc";
+
+  $.ajax({
+    url: queryUrl2,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    const coverThumbUrl = response.data.results[0].thumbnail.path;
+    console.log(coverThumbUrl);
+    $("#test").append(coverThumbUrl + "jpg");
+  });   
+}
+comicSearch();
